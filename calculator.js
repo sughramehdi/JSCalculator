6969e219e6dd84d0dcf1828dabb8d50bd3667311
sughramehdi <sughra.mehdi@gmail.com>
2020-10-13 15:15:55 +0500

    Added the calculator functionality.

--- calculator.js
+++ calculator.js
@@ -3,7 +3,8 @@
  */
 
 var string1 = '';
-var string2 = '';
+var numbers = [];
+var result = 0;
 
 $("button").click(function(){
     //displaying numbers
@@ -13,24 +14,58 @@ $("button").click(function(){
     }
     else {
         if ($(this).attr('id') == 'addButton'){
-            $("#output").html(string1 + "add");
+            string1 = $("#display").val();
+            numbers[0] = Number(string1);        
+            numbers[1] = 'add';
+            $("#output").html("add");
+            string1 = '';
         }
         if ($(this).attr('id') == 'subtractButton'){
+            string1 = $("#display").val();
+            numbers[0] = Number(string1);        
+            numbers[1] = 'subtract';
             $("#output").html("subtract");
+            string1 = '';
         }
         if ($(this).attr('id') == 'multiplyButton'){
+            string1 = $("#display").val();
+            numbers[0] = Number(string1);        
+            numbers[1] = 'multiply';
             $("#output").html("multiply");
+            string1 = '';
         }
         if ($(this).attr('id') == 'divideButton'){
+            string1 = $("#display").val();
+            numbers[0] = Number(string1);        
+            numbers[1] = 'divide'
             $("#output").html("divide");
+            string1 = '';
         }
         if ($(this).attr('id') == 'clearButton'){
             $("#output").html("clear");
             $('#display').val('');
             string1 = "";
+            numbers = [];
         }
         if ($(this).attr('id') == 'equalsButton'){
-            $("#output").html("equals");
+            const second = Number(string1);
+            if (numbers[1] == 'add') {
+                result = numbers[0] + second;
+            }
+            else {
+                if (numbers[1] == 'subtract') {
+                    result = numbers[0] - second;
+                }
+                if (numbers[1] == 'multiply') {
+                    result = numbers[0] * second;
+                }
+                if (numbers[1] == 'divide') {
+                    result = numbers[0] / second;
+                }
+            }
+            $('#display').val(result);
+            $("#output").html("equals " + result);
+            string1 = '';
         }
     }
 });
