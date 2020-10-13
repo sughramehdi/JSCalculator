04e5cb28178988b2467642a49fa052c94592a6af
sughramehdi <sughra.mehdi@gmail.com>
2020-10-13 16:06:12 +0500

    updated calculator.js for multiple operations without using equals button.

--- calculator.js
+++ calculator.js
@@ -3,41 +3,74 @@
  */
 
 var string1 = '';
-var numbers = [];
-var result = 0;
+var number1 = 0;
+var result = null;
+var operator = null;
 
 $("button").click(function(){
     //displaying numbers
     if ($(this).val()) {
-        string1 = string1.concat($(this).val());
-        $("#display").val(string1);
+        if (result){
+            string1 = '';
+            string1 = string1.concat($(this).val());
+            $("#display").val(string1);
+            result = null;
+        }
+        else {
+            string1 = string1.concat($(this).val());
+            $("#display").val(string1);
+        }
     }
     else {
         if ($(this).attr('id') == 'addButton'){
-            string1 = $("#display").val();
-            numbers[0] = Number(string1);        
-            numbers[1] = 'add';
+            if (operator) {
+                const second = Number($("#display").val());
+                number1 = calculate(number1, second, operator);
+                $("#display").val(number1);
+            }
+            else {
+                number1 = Number($("#display").val());        
+            }
+            operator = 'add'
             $("#output").html("add");
             string1 = '';
         }
         if ($(this).attr('id') == 'subtractButton'){
-            string1 = $("#display").val();
-            numbers[0] = Number(string1);        
-            numbers[1] = 'subtract';
+            if (operator) {
+                const second = Number($("#display").val());
+                number1 = calculate(number1, second, operator);
+                $("#display").val(number1);
+            }
+            else {
+                number1 = Number($("#display").val());        
+            }
+            operator = 'subtract';
             $("#output").html("subtract");
             string1 = '';
         }
         if ($(this).attr('id') == 'multiplyButton'){
-            string1 = $("#display").val();
-            numbers[0] = Number(string1);        
-            numbers[1] = 'multiply';
+            if (operator) {
+                const second = Number($("#display").val());
+                number1 = calculate(number1, second, operator);
+                $("#display").val(number1);
+            }
+            else {
+                number1 = Number($("#display").val());        
+            }
+            operator = 'multiply';
             $("#output").html("multiply");
             string1 = '';
         }
         if ($(this).attr('id') == 'divideButton'){
-            string1 = $("#display").val();
-            numbers[0] = Number(string1);        
-            numbers[1] = 'divide'
+            if (operator) {
+                const second = Number($("#display").val());
+                number1 = calculate(number1, second, operator);
+                $("#display").val(number1);
+            }
+            else {
+                number1 = Number($("#display").val());        
+            }
+            operator = 'divide'
             $("#output").html("divide");
             string1 = '';
         }
@@ -45,27 +78,35 @@ $("button").click(function(){
             $("#output").html("clear");
             $('#display').val('');
             string1 = "";
-            numbers = [];
+            number1 = 0;
+            operator = null;
+            result = null;
         }
         if ($(this).attr('id') == 'equalsButton'){
             const second = Number(string1);
-            if (numbers[1] == 'add') {
-                result = numbers[0] + second;
-            }
-            else {
-                if (numbers[1] == 'subtract') {
-                    result = numbers[0] - second;
-                }
-                if (numbers[1] == 'multiply') {
-                    result = numbers[0] * second;
-                }
-                if (numbers[1] == 'divide') {
-                    result = numbers[0] / second;
-                }
-            }
+            result = calculate (number1, second, operator);
             $('#display').val(result);
             $("#output").html("equals " + result);
             string1 = '';
+            operator = null;
         }
     }
 });
+
+function calculate(number1, number2, operator){
+    if (operator == 'add') {
+        result = number1 + number2;
+    }
+    else {
+        if (operator == 'subtract') {
+            result = number1 - number2;
+        }
+        if (operator == 'multiply') {
+            result = number1 * number2;
+        }
+        if (operator == 'divide') {
+            result = number1 / number2;
+        }
+    }
+    return result;
+}
